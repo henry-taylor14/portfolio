ff72d7e086ea193f7d032e553903cfc45f751dff
henry-taylor14 <73138585+henry-taylor14@users.noreply.github.com>
2026-01-26 10:50:09 -0800

    Fix about page

--- frontend/sanity/lib/live.ts
+++ frontend/sanity/lib/live.ts
@@ -14,5 +14,5 @@ export const {sanityFetch, SanityLive} = defineLive({
   // Required for showing draft content when the Sanity Presentation Tool is used, or to enable the Vercel Toolbar Edit Mode
   serverToken: token,
   // Required for stand-alone live previews, the token is only shared to the browser if it's a valid Next.js Draft Mode session
-  browserToken: token,
+  browserToken: undefined,
 })
